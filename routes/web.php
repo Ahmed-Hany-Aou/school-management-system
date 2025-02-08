43a07e65b69fc2b1f4fef7964208f5e1d6127586
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-02-09 01:19:37 +0200

    implemented exam type craud operations

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Backend\Setup\StudentGroupController;
 use App\Http\Controllers\Backend\Setup\StudentShiftController;
 use App\Http\Controllers\Backend\Setup\FeeCategoryController;
 use App\Http\Controllers\Backend\Setup\FeeAmountControllere;
-
+use App\Http\Controllers\Backend\Setup\ExamTypeController;
 
 
 
@@ -165,6 +165,19 @@ Route::get('fee/amount/details/{fee_category_id}', [FeeAmountControllere::class,
 
 
 
+// Exam Type Routes 
+
+Route::get('exam/type/view', [ExamTypeController::class, 'ViewExamType'])->name('exam.type.view');
+
+Route::get('exam/type/add', [ExamTypeController::class, 'ExamTypeAdd'])->name('exam.type.add');
+
+Route::post('exam/type/store', [ExamTypeController::class, 'ExamTypeStore'])->name('store.exam.type');
+
+Route::get('exam/type/edit/{id}', [ExamTypeController::class, 'ExamTypeEdit'])->name('exam.type.edit');
+
+Route::post('exam/type/update/{id}', [ExamTypeController::class, 'ExamTypeUpdate'])->name('update.exam.type');
+
+Route::get('exam/type/delete/{id}', [ExamTypeController::class, 'ExamTypeDelete'])->name('exam.type.delete');
 
 
 
