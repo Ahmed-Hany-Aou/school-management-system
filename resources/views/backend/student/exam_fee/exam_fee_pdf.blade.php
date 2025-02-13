807c98b5b25388b315c1ffa8361030dc69e46f61
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-02-13 13:57:11 +0200

    Fixed issue in exam fee operations by correctly passing the id  and displaying exam type in PDF view, and added middlewear function in our routes

--- resources/views/backend/student/exam_fee/exam_fee_pdf.blade.php
+++ resources/views/backend/student/exam_fee/exam_fee_pdf.blade.php
@@ -28,16 +28,12 @@
 </head>
 <body>
 
-
 <table id="customers">
   <tr>
     <td><h2>
-  <?php $image_path = '/upload/easyschool.png'; ?>
-  <img src="{{ public_path() . $image_path }}" width="200" height="100">
-
+  <?php $image_path = '/upload/oldhany.jpg'; ?>
+  <img src="{{ public_path() . $image_path }}" width="300" height="300">
     </h2></td>
-
-
     <td><h2>Easy School ERP</h2>
 <p>School Address</p>
 <p>Phone : 343434343434</p>
@@ -45,17 +41,14 @@
 <p> <b> Student Exam Fee</b> </p>
     </td> 
   </tr>
-  
-   
 </table>
 
 @php 
 $registrationfee = App\Models\FeeCategoryAmount::where('fee_category_id','1')->where('class_id',$details->class_id)->first();
 $originalfee = $registrationfee->amount;
-        $discount = $details['discount']['discount'];
-        $discounttablefee = $discount/100*$originalfee;
-        $finalfee = (float)$originalfee-(float)$discounttablefee;
-
+$discount = $details['discount']['discount'];
+$discounttablefee = $discount/100*$originalfee;
+$finalfee = (float)$originalfee-(float)$discounttablefee;
 @endphp 
 
 <table id="customers">
@@ -74,14 +67,11 @@ $originalfee = $registrationfee->amount;
     <td><b>Roll No</b></td>
     <td>{{ $details->roll }}</td>
   </tr>
-
-    <tr>
+  <tr>
     <td>3</td>
     <td><b>Student Name</b></td>
     <td>{{ $details['student']['name'] }}</td>
   </tr>
-
-   
   <tr>
     <td>5</td>
     <td><b>Session</b></td>
@@ -102,80 +92,16 @@ $originalfee = $registrationfee->amount;
     <td><b>Discount Fee </b></td>
     <td>{{ $discount  }} %</td>
   </tr>
-
-    <tr>
+  <tr>
     <td>9</td>
     <td><b>Fee For this Student of {{ $exam_type }} </b></td>
     <td>{{ $finalfee }} $</td>
   </tr>
- 
-    
-   
 </table>
 <br> <br>
-  <i style="font-size: 10px; float: right;">Print Data : {{ date("d M Y") }}</i>
-
-<hr style="border: dashed 2px; width: 95%; color: #000000; margin-bottom: 50px">
-
-<table id="customers">
-  <tr>
-    <th width="10%">Sl</th>
-    <th width="45%">Student Details</th>
-    <th width="45%">Student Data</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td><b>Student ID No</b></td>
-    <td>{{ $details['student']['id_no'] }}</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td><b>Roll No</b></td>
-    <td>{{ $details->roll }}</td>
-  </tr>
-
-    <tr>
-    <td>3</td>
-    <td><b>Student Name</b></td>
-    <td>{{ $details['student']['name'] }}</td>
-  </tr>
-
-  
-  <tr>
-    <td>5</td>
-    <td><b>Session</b></td>
-    <td>{{ $details['student_year']['name'] }}</td>
-  </tr>
-  <tr>
-    <td>6</td>
-    <td><b>Class </b></td>
-    <td>{{ $details['student_class']['name'] }}</td>
-  </tr>
-  <tr>
-    <td>7</td>
-    <td><b>Exam Fee</b></td>
-    <td>{{ $originalfee }} $</td>
-  </tr>
-  <tr>
-    <td>8</td>
-    <td><b>Discount Fee </b></td>
-    <td>{{ $discount  }} %</td>
-  </tr>
-
-    <tr>
-    <td>9</td>
-   <td><b>Fee For this Student of {{ $exam_type }} </b></td>
-    <td>{{ $finalfee }} $</td>
-  </tr>
- 
-    
-   
-</table>
-<br> <br>
-  <i style="font-size: 10px; float: right;">Print Data : {{ date("d M Y") }}</i>
-
-
-
+<i style="font-size: 10px; float: right;">Print Data : {{ date("d M Y") }}
+  <hr style="border: dashed 2px; width: 95%; color: #000000; margin-bottom: 50px">
+</i>
 
 
 
