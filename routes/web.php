3a7411c1da486f1cdedc263828e0f825cbedf92b
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-02-14 19:52:30 +0200

    Implemented employee salary functions and joined/added job name in employee table. This was done to ensure understanding of data retrieval from different tables. (Out of scope project)

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\Backend\Student\MonthlyFeeController;
 use App\Http\Controllers\Backend\Student\ExamFeeController;
 
 use App\Http\Controllers\Backend\Employee\EmployeeRegController;
+use App\Http\Controllers\Backend\Employee\EmployeeSalaryController;
 
 
 /*
@@ -313,6 +314,14 @@ Route::prefix('employees')->group(function(){
     
     Route::get('reg/employee/details/{id}', [EmployeeRegController::class, 'EmployeeDetails'])->name('employee.registration.details');
     
+// Employee Salary All Routes 
+Route::get('salary/employee/view', [EmployeeSalaryController::class, 'SalaryView'])->name('employee.salary.view');
+Route::get('salary/employee/increment/{id}', [EmployeeSalaryController::class, 'SalaryIncrement'])->name('employee.salary.increment');
+Route::post('salary/employee/store/{id}', [EmployeeSalaryController::class, 'SalaryStore'])->name('update.increment.store');
+Route::get('salary/employee/details/{id}', [EmployeeSalaryController::class, 'SalaryDetails'])->name('employee.salary.details');
+
+
+
     }); 
  
 
